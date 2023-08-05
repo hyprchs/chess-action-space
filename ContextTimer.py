298f3ddf36a3e2985008c92101db1570488b1b41
Jackson Hall <jacksonthall22@gmail.com>
2023-08-05 14:39:18 -0400

    Moves -> binary by indexing all possible actions from square, pseudo-legal moves, or legal moves

---
+++ ContextTimer.py
@@ -0,0 +1,16 @@
+from time import perf_counter
+
+
+class ContextTimer:
+
+    def __init__(self, precision: int = 3):
+        self.precision = precision
+
+    def __enter__(self):
+        self.time = perf_counter()
+        return self
+
+    def __exit__(self, type, value, traceback):
+        self.time = perf_counter() - self.time
+        self.readout = f'Time: {self.time:.{self.precision}f} seconds'
+        print(self.readout)
